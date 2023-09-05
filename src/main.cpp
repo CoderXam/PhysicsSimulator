2ad90b0709f55752dcd6606a3639aa022194198f
CoderXam <maxdaniellevine@gmail.com>
2023-09-05 15:02:29 -0400

    Hello World

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
+    std::cout << "Hello World" << std::endl;
+
+    // hello world triangle
+    sf::VertexArray triangle(sf::Triangles, 3);
+    triangle[0].position = sf::Vector2f(10.f, 10.f);
+    triangle[1].position = sf::Vector2f(100.f, 10.f);
+    triangle[2].position = sf::Vector2f(100.f, 100.f);
+    triangle[0].color = sf::Color::Red;
+    triangle[1].color = sf::Color::Blue;
+    triangle[2].color = sf::Color::Green;
+
     auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
     window.setFramerateLimit(144);
 
@@ -16,6 +28,7 @@ int main()
         }
 
         window.clear();
+        window.draw(triangle);
         window.display();
     }
 }
